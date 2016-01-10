e10b3a3f9ee25b6cb1ed396c6b7f4c672e59e717
Jorge Moreno <alterebro@gmail.com>
2016-01-10 19:03:41 +0100

    social networks sharing links

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,3 +1,4 @@
+// Utils
 function foreach(selector, fn) {
 	var elements = document.querySelectorAll(selector);
 	for (var i = 0; i < elements.length; i++) {
@@ -31,6 +32,39 @@ function removeclass(el, classname) {
 	}
 }
 
+// Social Networks Sharing
+function share(network) {
+	var config = {
+		url : 'http://alterebro.github.io/armazon.css/',
+		title : 'armazon.css - lightweight and super simple CSS3 Boilerplate Micro Framework built on LESS',
+		content : 'Minimal and supersimple CSS Boilerplate Micro Framework that provides a customizable adaptive grid system, cross browser consistency and improved styling for the standard HTML elements.',
+		image : 'http://alterebro.github.io/armazon.css/www/images/armazon-css.jpg'
+	}
+
+    var network_base_urls = {
+        facebook: "http://www.facebook.com/sharer.php?u={url}&t={title}",
+        twitter: "http://twitter.com/share?text={title}&url={url}",
+        googleplus: "https://plusone.google.com/_/+1/confirm?hl=en&url={url}",
+        linkedin: "http://www.linkedin.com/shareArticle?mini=true&url={url}&title={title}&summary={content}",
+        tumblr: "http://www.tumblr.com/share?v=3&u={url}",
+        pinterest: "http://pinterest.com/pin/create/button/?url={url}&media=&description={content}",
+		email: "mailto:?subject={title}&body={content}{url}"
+    };
+
+	var network_url = network_base_urls[network].replace(/\{url\}/, encodeURIComponent(config.url))
+       .replace(/\{title\}/, encodeURIComponent(config.title))
+       .replace(/\{content\}/, encodeURIComponent(config.content))
+       .replace(/\{image\}/, encodeURIComponent(config.image));
+
+	if ( network == 'email' ) {
+		window.location.href = network_url;
+	} else {
+		var network_window = window.open(network_url, network+'-share', 'height=350,width=600');
+			network_window.focus();
+	}
+}
+
+// URL hash routing
 function check_url_hash() {
 	var h = window.location.hash.substring(1).split('.');
 		hash = h[0];
@@ -87,6 +121,15 @@ window.onload = function() {
 		else { window.location.hash = '#'+sections[0]; }
 		window.setTimeout(function(){ document.body.scrollTop = document.documentElement.scrollTop = 0; }, 1);
 	}
+
+	// Share Links
+	foreach('#social-sharing li a', function(el, i){
+		el.onclick = function(e) {
+			e.preventDefault();
+			share( this.getAttribute('class') );
+		}
+	});
+
 }
 
 window.onhashchange = function(e) { check_url_hash(); }
